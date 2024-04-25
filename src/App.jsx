e6b5083a3c4c52650c829c22278870fb863405fd
wecreus <wecreus@gmail.com>
2024-04-25 20:08:56 +0300

    beginning

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.scss";
+import CoolArrow from "./assets/icons/arrow.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+    <div className="wrapper">
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+        <p className="card__text">
+          <span className="card__commoner">theCommoner</span>is my humble
+          library of thoughts 💭, game reviews 👾, and photos 📸, providing me
+          with the opportunity to sharpen my skills as a developer👨🏻‍💻
         </p>
+        <div className="card__arrow">
+          <img src={CoolArrow} alt="" className="card__arrow-img" />
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
