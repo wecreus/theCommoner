12e7c15a42f2119a8e780e6b9bbdedd20605ffd3
wecreus <wecreus@gmail.com>
2024-05-03 20:52:36 +0300

    icon change

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,14 @@ import Gallery from "./MainComponents/Gallery";
 import ReactPageScroller from "react-page-scroller";
 
 function App() {
+  const [pageNumber, setPageNumber] = useState(0);
+  const handlePageChange = (page) => {
+    setPageNumber(page);
+  };
+
   return (
-    <ReactPageScroller animationTimer={350}>
-      <Welcome />
+    <ReactPageScroller animationTimer={350} customPageNumber={pageNumber}  pageOnChange={handlePageChange}>
+      <Welcome onScrollClick={() => setPageNumber(1)}/>
       <Gallery />
     </ReactPageScroller>
   );
--- src/MainComponents/Welcome.jsx
+++ src/MainComponents/Welcome.jsx
@@ -4,16 +4,24 @@ import AlienMonster from "../assets/icons/AlienMonster.webp";
 import Technologist from "../assets/icons/Technologist.webp";
 import Thoughts from "../assets/icons/Thoughts.webp";
 import Fireworks from "../assets/icons/Fireworks.webp";
+import { ReactTyped } from "react-typed";
 
-function App() {
+function App(props) {
+    const {onScrollClick} = props;
+
   return (
     <div className="card">
       <p className="card__text">
-        <span className="commoner">theCommoner</span>is my humble library
-        of thoughts <img src={Thoughts} alt="" className="emoji" />, game reviews <img src={AlienMonster} alt="" className="emoji" />, and photos <img src={Fireworks} alt="" className="emoji" />, providing me with the
-        opportunity to sharpen my skills as a developer <img src={Technologist} alt="" className="emoji" />
+        <span className="commoner">
+          <ReactTyped strings={["THE", "theCommoner"]} typeSpeed={100} backSpeed={20}/>
+        </span>is my humble library of thoughts{" "}
+        <img src={Thoughts} alt="" className="emoji" />, game reviews{" "}
+        <img src={AlienMonster} alt="" className="emoji" />, and photos{" "}
+        <img src={Fireworks} alt="" className="emoji" />, providing me with the
+        opportunity to sharpen my skills as a developer{" "}
+        <img src={Technologist} alt="" className="emoji" />
       </p>
-      <div className="card__arrow">
+      <div className="card__arrow" onClick={onScrollClick}>
         <img src={CoolArrow} alt="" className="card__arrow-img" />
       </div>
     </div>
