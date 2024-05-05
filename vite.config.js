0893e95812b76737c38ef73a5ee9309625fbb561
wecreus <wecreus@gmail.com>
2024-05-05 15:57:41 +0300

    transformEsModules on build

--- vite.config.js
+++ vite.config.js
@@ -18,4 +18,7 @@ export default defineConfig({
     // necessary for segment analytics lib to work
     global: {},
   },
+  build: {
+    commonjsOptions: { transformMixedEsModules: true }
+  }
 });
