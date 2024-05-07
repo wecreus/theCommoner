bc666047d15560506a6b68199901e1298e432063
wecreus <wecreus@gmail.com>
2024-05-07 11:47:48 +0300

    testing gallery scroll with react-page-scroller

--- src/layout/HomeLayout/HomeComponents/Gallery.jsx
+++ src/layout/HomeLayout/HomeComponents/Gallery.jsx
@@ -1,16 +1,42 @@
-function Gallery() {
+import { useEffect } from "react";
+import { Carousel } from "react-responsive-carousel";
+import { GalleryPictures } from "@/common/utils";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+const Gallery = () => {
+  useEffect(() => {
+    document.addEventListener("touchmove", (e) => {
+
+    });
+  }, []);
   return (
     <section className="card">
-
+      <Carousel
+        emulateTouch
+        showArrows={false}
+        showIndicators={false}
+        showThumbs={false}
+        showStatus={false}
+        className="Gallery-carousel"
+      >
         <img
-          src="https://live.staticflickr.com/65535/53702745672_469946719a_k.jpg"
+          src={GalleryPictures[0].url}
           width="400px"
           alt="Best"
         />
-   
-
+        <img
+          src={GalleryPictures[1].url}
+          width="400px"
+          alt="Best"
+        />
+        <img
+          src={GalleryPictures[2].url}
+          width="400px"
+          alt="Best"
+        />
+      </Carousel>
     </section>
   );
-}
+};
 
 export default Gallery;
