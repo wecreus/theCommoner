a584768c7127dde00d9958fd9769173df7c7215c
wecreus <wecreus@gmail.com>
2024-06-17 06:14:17 +0300

    feat: vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import Router from "./routes/Router";
 import { Provider } from "react-redux";
 import store from "./store";
 import "./index.scss";
+import { inject } from '@vercel/analytics';
+ 
+inject();
 
 // TODO: make a version of the website with either Next.js or React Server Components
 ReactDOM.createRoot(document.getElementById("root")).render(
